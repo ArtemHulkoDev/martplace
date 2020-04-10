4294ce654dd2cb456586776535bcc0e39643db1e
ArtemGulkoDev <agulko96@gmail.com>
2020-04-10 17:49:03 +0300

    added weekly products slider

---
+++ app/js/main.js
@@ -0,0 +1,10 @@
+$(function(){
+
+  $('.weeklyproducts__slider').slick({
+    dots: false,
+    arrows: true,
+    prevArrow: '<button class="slick-arrow slick-prev"><img src="images/icons/arrow-left.png" alt=""></button>',
+    nextArrow: '<button class="slick-arrow slick-next"><img src="images/icons/arrow-right.png" alt=""></button>',
+  });
+  
+});
