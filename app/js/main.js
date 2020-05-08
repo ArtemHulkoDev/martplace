3ec950f583d389d78bbfd06f91dd79dc797de21f
ArtemGulkoDev <agulko96@gmail.com>
2020-05-08 20:52:05 +0300

    added newproducts section and followers section

--- app/js/main.js
+++ app/js/main.js
@@ -6,5 +6,18 @@ $(function(){
     prevArrow: '<button class="slick-arrow slick-prev"><img src="images/icons/arrow-left.png" alt=""></button>',
     nextArrow: '<button class="slick-arrow slick-next"><img src="images/icons/arrow-right.png" alt=""></button>',
   });
+
+  $('.followers__slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots: false,
+    arrows: true,
+    variableWidth: true,
+    prevArrow: '<button class="slick-arrow slick-prev"><img src="images/icons/arrow-left.png" alt=""></button>',
+    nextArrow: '<button class="slick-arrow slick-next"><img src="images/icons/arrow-right.png" alt=""></button>'
+  });
+
+  var mixer = mixitup('.newproducts__box');
   
 });
